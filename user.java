f95893c5ab7a95034d94c2c867f7eca13cd64476
smansura5408-syed <smansura5408@gmail.com>
2026-08-07 17:24:01 +0530

    Update user.java

--- user.java
+++ user.java
@@ -1,74 +1,162 @@
 package com.hospital.app.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "users")
+@Table(name="user")
 public class User {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "full_name")
-    private String fullName;
 
     private String username;
 
-    private String password;
+    private String email;
 
-    private String phone;
+    private String password;
 
     private String role;
 
-    public User() {
+
+    @Column(name="hospital_id")
+    private Integer hospitalId;
+
+
+    @Column(name="hospital_name")
+    private String hospitalName;
+
+
+    private String name;
+
+
+    @Column(name="full_name")
+    private String fullName;
+
+
+    private String phone;
+
+
+
+    public User(){
+
     }
+
+
 
     public int getId() {
         return id;
     }
 
+
     public void setId(int id) {
-        this.id = id;
+        this.id=id;
     }
 
-    public String getFullName() {
-        return fullName;
-    }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
 
     public String getUsername() {
         return username;
     }
 
+
     public void setUsername(String username) {
-        this.username = username;
+        this.username=username;
     }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email=email;
+    }
+
+
 
     public String getPassword() {
         return password;
     }
 
+
     public void setPassword(String password) {
-        this.password = password;
+        this.password=password;
     }
 
-    public String getPhone() {
-        return phone;
-    }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 
     public String getRole() {
         return role;
     }
 
+
     public void setRole(String role) {
-        this.role = role;
+        this.role=role;
     }
+
+
+
+    public Integer getHospitalId() {
+        return hospitalId;
+    }
+
+
+    public void setHospitalId(Integer hospitalId) {
+        this.hospitalId=hospitalId;
+    }
+
+
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName=hospitalName;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name=name;
+    }
+
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+
+    public void setFullName(String fullName) {
+        this.fullName=fullName;
+    }
+
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+
+    public void setPhone(String phone) {
+        this.phone=phone;
+    }
+
 }
