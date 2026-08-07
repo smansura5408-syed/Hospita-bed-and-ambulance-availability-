182257a86183569deb400d31b8768433605b170f
smansura5408-syed <smansura5408@gmail.com>
2026-08-07 17:25:52 +0530

    Update HospitalRepository.java

--- HospitalRepository.java
+++ HospitalRepository.java
@@ -1,8 +1,16 @@
 package com.hospital.app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.hospital.app.entity.Hospital;
 
+@Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+
+    // Search hospitals by location
+    List<Hospital> findByLocationContainingIgnoreCase(String location);
 
 }
