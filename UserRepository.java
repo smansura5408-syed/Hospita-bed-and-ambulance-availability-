adb0d53ea9cf3df90470d48fb87befca459a14c6
smansura5408-syed <smansura5408@gmail.com>
2026-08-07 17:26:22 +0530

    Update UserRepository.java

--- UserRepository.java
+++ UserRepository.java
@@ -1,10 +1,13 @@
 package com.hospital.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.hospital.app.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUsername(String username);
+
+    User findByEmail(String email);
 
 }
